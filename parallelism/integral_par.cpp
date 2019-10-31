c0bfc4d62928950bf31b068b8fb62a80ee1cb2d1
Katharina von Sturm <student27@hpc-201-11-40.cr.cnaf.infn.it>
2019-10-31 09:26:22 +0100

    parallel for

--- parallelism/integral_par.cpp
+++ parallelism/integral_par.cpp
@@ -1,27 +1,48 @@
-#include <threads>
+#include <thread>
 #include <atomic>
+#include <tbb/tbb.h>
+#include <iostream>
+#include <execution>
+
+template<class T>
+void dump_vector(std::vector<T> v)
+{
+  std::cout << "{";
+  for(auto vi : v)
+    std::cout << " " << vi;
+  std::cout << " }" << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
   if(argc < 3)
   {
+    std::cout << "Not enough arguments : " << argc << std::endl;
     return 1;
   }
 
-  unsigned int nbins = argv[1];
-  float limit_l      = argv[2];
-  float limit_r      = argv[3];
+  unsigned int nbins = atoi(argv[1]);
+  float limit_l      = atof(argv[2]);
+  float limit_r      = atof(argv[3]);
   float step = (limit_r-limit_l)/nbins;
-
-  // define a stupid function f(x) = x;
-  auto f = [](float val){ return val; };
 
   // integrate in parallel
   std::atomic<float> integral(0);
 
-  std::vector<int> v(nbins);
+  std::vector<float> v(nbins);
   int c = 0;
   for( auto & vi : v ) vi = step*c++;
 
-  std::transform(std::execute::par, v.begin(), v.end(), f);
+  dump_vector(v);
+  std::for_each(std::execution::par, v.begin(), v.end(), [](float &val){val*=2;});
+  dump_vector(v);
+
+  double sum = 0;
+
+  tbb::parallel_for(
+    tbb::blocked_range<int>(0,nbins),
+    [&](const tbb::blocked_range<int>& range) { for(int i = range.begin(); i < range.end(); ++i){ sum += v[i]*step;}  }
+  );
+
+  std::cout << "Integral : " << sum << std::endl;
 }
